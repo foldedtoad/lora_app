9dd205c2b01fd267cbebfb61e5d2b0744f6e4018
foldedtoad <foldedtoad@gmail.com>
2020-05-21 19:38:56 -0700

    initial commit

---
+++ dts_fixup.h
@@ -0,0 +1,7 @@
+/*
+ *
+ */
+#if defined(DT_RTC_0_NAME)
+  #undef DT_RTC_0_NAME
+  #define DT_RTC_0_NAME			DT_NORDIC_NRF_RTC_RTC_2_LABEL
+#endif
---
+++ inc/lora_app.h
@@ -0,0 +1,10 @@
+/*
+ *  lora_app.h
+ */
+#ifndef __LORA_APP_H__
+#define __LORA_APP_H__
+
+int  lora_app_init(void);
+void lora_app_send(void);
+
+#endif  // __LORA_APP_H__
