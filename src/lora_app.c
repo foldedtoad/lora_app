82ad6513fa421df94f072b9b5f024ff832ddd276
foldedtoad <foldedtoad@gmail.com>
2020-05-22 22:19:46 -0700

    add alternate configuration

--- src/lora_app.c
+++ src/lora_app.c
@@ -16,9 +16,16 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER(lora_app);
 
+#if 1
 #define MAX_DATA_LEN 10
-
 char data[MAX_DATA_LEN] = {'h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd'};
+#else
+#define MAX_DATA_LEN 16
+#define FROM_ID 1
+#define TO_ID   2
+char data[MAX_DATA_LEN] = {TO_ID, FROM_ID, 0, 0, 
+                  'h', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd'};
+#endif
 
 static struct device * lora_dev;
 
@@ -36,6 +43,7 @@ int lora_app_init(void)
         return -1;
     }
 
+#if 1
     config.frequency = 904300000; // FIXME make this a parameter.
     config.bandwidth = BW_125_KHZ;
     config.datarate = SF_7;
@@ -43,6 +51,15 @@ int lora_app_init(void)
     config.coding_rate = CR_4_5;
     config.tx_power = 21;
     config.tx = true;
+#else
+    config.frequency = 915000000;    // FIXME make this a parameter.
+    config.bandwidth = BW_125_KHZ;
+    config.datarate = SF_7;
+    config.preamble_len = 8;
+    config.coding_rate = CR_4_5;
+    config.tx_power = 4;
+    config.tx = true;
+#endif
 
     ret = lora_config(lora_dev, &config);
     if (ret < 0) {
