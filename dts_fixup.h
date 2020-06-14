982e9b698611f705a3f2792ee42f4a67acaed49b
foldedtoad <foldedtoad@gmail.com>
2020-06-13 17:26:42 -0700

    configure for nucleo_l476rg

--- dts_fixup.h
+++
@@ -1,7 +0,0 @@
-/*
- *
- */
-#if defined(DT_RTC_0_NAME)
-  #undef DT_RTC_0_NAME
-  #define DT_RTC_0_NAME			DT_NORDIC_NRF_RTC_RTC_2_LABEL
-#endif
--- src/main.c
+++ src/main.c
@@ -10,67 +10,7 @@ LOG_MODULE_REGISTER(main, 3);
 #define STACKSIZE 1024
 #define PRIORITY 7
 
-int LoRa_init( void );
 
-//#ifdef CONFIG_BT
-#if 0
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*---------------------------------------------------------------------------*/
-#include "ble_policy.h"
-#include "ble_base.h"
-
-void bluetooth_thread(void * id, void * unused1, void * unused2)
-{
-    LOG_INF("%s", __func__);
-
-    ble_policy_init();
-
-    while(1) { /* spin */}
-}
-
-K_THREAD_DEFINE(bluetooth_id, STACKSIZE, bluetooth_thread, 
-                NULL, NULL, NULL, PRIORITY, 0, K_NO_WAIT);
-#endif
-
-#ifdef CONFIG_LORA
-#include "lora_app.h"
-
-#ifdef LORA_APP_TX_MODE
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*---------------------------------------------------------------------------*/
-void lora_send_thread(void * id, void * unused1, void * unused2)
-{
-    LOG_INF("%s", __func__);
-
-    if (lora_app_init() == 0) {
-        lora_app_send();  // never returns
-    }
-}
-
-K_THREAD_DEFINE(lora_send_id, STACKSIZE, lora_send_thread, 
-                NULL, NULL, NULL, PRIORITY, 0, K_NO_WAIT);
-#endif
-
-#ifndef LORA_APP_TX_MODE
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*---------------------------------------------------------------------------*/
-void lora_receive_thread(void * id, void * unused1, void * unused2)
-{
-    LOG_INF("%s", __func__);
-
-    if (lora_app_init() == 0) {
-        lora_app_receive();  // never returns
-    }
-}
-
-K_THREAD_DEFINE(lora_receive_id, STACKSIZE, lora_receive_thread, 
-                NULL, NULL, NULL, PRIORITY, 0, K_NO_WAIT);
-#endif
-
-#endif // CONFIG_LORA
 
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
@@ -81,7 +21,6 @@ void main_thread(void * id, void * unused1, void * unused2)
 
     k_sleep( K_MSEC(500));
 
-   //ble_start_advertising();
 }
 
 K_THREAD_DEFINE(main_id, STACKSIZE, main_thread, 
